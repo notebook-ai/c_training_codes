adf5e654d76c24297b7e356dee2d0e6ba3269355
notebook-ai <m.hosseinzadeh.ai@gmail.com>
2024-09-06 17:24:05 +0330

    .

---
+++ 05_variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int num = 45;
+	float avg = 17.83;
+	char character =  'n';
+	char word[] = "smile";
+	
+	printf("int num: %d\n", num);
+	printf("float avg: %.2f\n", avg);
+	printf("char character: %c\n", character);
+	printf("char word: %s\n", word);
+	
+	return 0;
+}
